523dab09db0f5a5b55e9c67d862dadee879c8988
TheMat556 <79401695+TheMat556@users.noreply.github.com>
2023-06-06 23:22:57 +0200

    minor changes

--- src/app/spotify.service.ts
+++ src/app/spotify.service.ts
@@ -3,17 +3,18 @@ import {HttpClient} from "@angular/common/http";
 
 import credentials from "../assets/credentials.json";
 import {lastValueFrom} from "rxjs";
+import {RoomService} from "./room.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private roomServie: RoomService) {}
 
   loginIntoSpotify () {
     const response = this.http.get(
-      credentials.baseUri + credentials.endpoints.spotify.login,
+      credentials.baseUri + credentials.endpoints.spotify.login + "?roomIdentifier=" + this.roomServie.room.roomIdentifier,
       {withCredentials: true}
     )
     return lastValueFrom(response)
